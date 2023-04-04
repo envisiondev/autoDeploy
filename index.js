7194b79efb85264297e905b78b5b5a6ece9ad2aa
Hayden <63392839+envisiondev@users.noreply.github.com>
2023-04-04 15:40:45 -0500

    new port - pm2 update

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express')
 const app = express()
 
 app.post('/', (_, res) => {
-  exec(`cd "${process.argv.slice(2)}" && git pull && pm2 restart discord`)
+  exec(`cd "${process.argv.slice(2)}" && git pull && pm2 restart 0`)
   res.sendStatus(200)
 })
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5028
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
